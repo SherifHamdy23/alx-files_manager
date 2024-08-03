25c2a809d0301e348cf2a52a9ef4fdba047d9896
SherifHamdy23 <sherifhamdy.dev@gmail.com>
2024-08-03 11:34:07 +0300

    3. Create a new user

---
+++ controllers/UsersController.js
@@ -0,0 +1,39 @@
+import { createHash } from 'crypto';
+import dbClient, { DBClient } from '../utils/db';
+
+
+function hashPassword(value) {
+    const hash = createHash('sha1');
+    hash.update(value);
+    return hash.digest('hex');
+}
+
+async function postNew(req, res) {
+    const data = req.body;
+
+    // ensure mongo is connected 
+    const { instance, db } = await DBClient.getInstance();
+    if (!data.email)
+        return res.status(400).send({"error":"Missing email"});
+    if (!data.password)
+        return res.status(400).send({"error":"Missing password"});
+    const user = await db.collection('users').findOne({email: data.email});
+    if (user) {
+        return res.status(400).send({"error":"Already exist"});
+    }
+    const users = db.collection('users');
+    const newUser = await users.insertOne({
+            email: data.email,
+            password: hashPassword(data.password)
+        });
+    return res.send({
+        email: data.email,
+        id: newUser.insertedId
+    });
+}
+
+const UsersController = {
+    postNew
+};
+
+export default UsersController;
