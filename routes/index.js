ce4745816c3f90c321752745ced50d64656c5037
SherifHamdy23 <sherifhamdy.dev@gmail.com>
2024-08-05 03:06:55 +0300

    fix eslint errors

--- routes/index.js
+++ routes/index.js
@@ -16,10 +16,12 @@ routes.get('/disconnect', AuthController.getDisconnect);
 routes.get('/users/me', AuthController.getMe);
 
 routes.post('/files', FilesController.postUpload);
-routes.get('/files/:id', FilesController.getShow)
-routes.get('/files', FilesController.getIndex)
+routes.get('/files/:id', FilesController.getShow);
+routes.get('/files', FilesController.getIndex);
 
 routes.put('/files/:id/publish', FilesController.putPublish);
-routes.put('/files/:id/unpublish', FilesController.putUnpublish)
+routes.put('/files/:id/unpublish', FilesController.putUnpublish);
+
+routes.get('/files/:id/data', FilesController.getFile);
 
 export default routes;
