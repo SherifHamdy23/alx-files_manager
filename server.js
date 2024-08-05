af10127276a19804f9fd99fe692e841a28fa11b3
SherifHamdy23 <sherifhamdy.dev@gmail.com>
2024-08-05 03:16:59 +0300

    port set by enviroment variable

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.listen(5000, () => {
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
   console.log('App listening in port 5000');
 });
 
